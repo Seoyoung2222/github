081388f4417182f394247d2d8b394d3deead991f
박서영 <127370004+Seoyoung2222@users.noreply.github.com>
2025-02-27 17:11:20 +0900

    [Silver I] Title: IOIOI, Time: 472 ms, Memory: 31932 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -3,27 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); // O의 갯수
-        int m = sc.nextInt(); // s의 길이
+        int n = sc.nextInt(); // IOI 패턴 갯수
+        int m = sc.nextInt(); // 문자열 길이
         sc.nextLine();
-        String strings = sc.nextLine();
-
-        char[] chars = strings.toCharArray();
+        String s = sc.nextLine();
 
         int result = 0;
-        for (int i = 0; i < m-(2*n); i++) {
-            if (chars[i] == 'I') {
-                boolean flag = true;
-                for (int j = 1; j <= n*2; j++) {
-                    if (j%2==1 && chars[i+j]=='O') {
-                        continue;
-                    } else if (j%2==0 && chars[i+j]=='I') {
-                        continue;
-                    } else {
-                        flag = false;
-                    }
+        int count = 0;
+
+        for (int i = 0; i < m-1; i++) {
+            if (s.charAt(i) == 'I' && i+2<m && s.charAt(i+1) == 'O' && s.charAt(i+2) == 'I') {
+                count++;
+                if (count >= n) {
+                    result++;
                 }
-                if (flag) result++;
+                i++; // 'OI'를 스킵하고 다음 카운트 시작
+            } else {
+                count = 0;
             }
         }
         System.out.println(result);
